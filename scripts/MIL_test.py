c7b2dc5e8ccd80b3210c3c82617cc18d6dd4239a
Burlutskiy, Nikolay <nikolay.burlutskiy@astrazeneca.com>
2021-06-02 15:33:44 +0100

    some mil tests

--- scripts/MIL_test.py
+++ scripts/MIL_test.py
@@ -3,9 +3,10 @@ import random
 import numpy as np
 from mil.data.datasets import musk1
 from mil.metrics import AUC, BinaryAccuracy
+from mil.utils.padding import Padding
 from mil.validators import KFold
 from mil.trainer.trainer import Trainer
-from mil.models import LogisticRegression, SVC
+from mil.models import LogisticRegression, SVC, AttentionDeepPoolingMil
 from mil.bag_representation.mapping import DiscriminativeMapping, MILESMapping
 from mil.preprocessing import StandarizerBagsList
 from sklearn.model_selection import train_test_split
@@ -81,7 +82,57 @@ def mil_test(path_to_data, use_smaller_bags=True, n_patches_max=100):
     history = trainer.fit(X_train, y_train, sample_weights='balanced', validation_strategy=valid, verbose=1)
 
 
+def mil_dl_test(path_to_data, n_epochs=10, batch_size=5, use_smaller_bags=True, n_patches_max=100):
+
+    bags_subset = pickleOpen(path_to_data + 'bags_subset.pkl')
+    labels_subset = pickleOpen(path_to_data + 'labels_subset.pkl')
+
+    X_train, X_test, y_train, y_test = train_test_split(bags_subset, labels_subset, stratify=labels_subset)
+    X_train = [l.tolist() for l in X_train]
+    X_test = [l.tolist() for l in X_test]
+
+    for train_bag in X_train:
+        print('%d samples in a train bag' % len(train_bag))
+        print('Embeddings are of %d size' % len(train_bag[0]))
+
+    # to speed up training, one can limit max number of patches per WSI:
+    if use_smaller_bags:
+        X_train_small = []
+        for train_bag in X_train:
+            print('%d samples in a bag' % len(train_bag))
+            new_bag = random.sample(train_bag, n_patches_max)
+            X_train_small.append(new_bag)
+        X_train = X_train_small
+
+    # maximum  number of instances in the training set
+    max_len_train = np.max([len(bag) for bag in X_train])
+    max_len_test = np.max([len(bag) for bag in X_test])
+
+    max_ = np.max([max_len_train, max_len_test])
+    max_
+
+    trainer = Trainer()
+    metrics = [AUC, BinaryAccuracy]
+    model = AttentionDeepPoolingMil(gated=False, threshold=0.4)
+    pipeline = [('padding', Padding(max_len=max_))]
+    trainer.prepare(model, preprocess_pipeline=pipeline, metrics=metrics)
+    # there are no many positive examples (only one?), you can see it if printing y_train vector:
+    print(y_train)
+    # this means that with Kfold where n=2 you end up with a training without positive examples
+    # you can fix it by a. validation_strategy=None b. include enough positive samples; this can be done by increasing
+    # the training dataset or by oversampling the underrepresenting dataset, for example, duplicate positive samples
+    #y_train = np.array([0, 0, 0, 0, 1, 1, 1])
+    valid = KFold(n_splits=2, shuffle=True)
+    history = trainer.fit(X_train, y_train,
+                          model__epochs=n_epochs,
+                          model__batch_size=batch_size,
+                          sample_weights='balanced',
+                          validation_strategy=valid,
+                          verbose=1)
+
+
 # an axample what must work
 #mil_example()
 # wrapped the function for training on the embeddings
-mil_test(path_to_data='/Users/kmwx127/Downloads/data_for_ML/', use_smaller_bags=True, n_patches_max=100)
+#mil_test(path_to_data='/Users/kmwx127/Downloads/data_for_ML/', use_smaller_bags=True, n_patches_max=100)
+mil_dl_test(path_to_data='/Users/kmwx127/Downloads/data_for_ML/', n_epochs=10, batch_size=5, use_smaller_bags=True, n_patches_max=100)
